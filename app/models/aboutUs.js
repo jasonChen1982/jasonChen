26499e4f637fdc11ce9ae6f99a5d514102fa2326
jason chen <cjx89807@alibaba-inc.com>
2017-02-19 22:49:27 +0800

    feat: egg init

--- app/models/aboutUs.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose')
-var AboutUsSchema = require('../schemas/admin')
-var AboutUs = mongoose.model('AboutUs',AboutUsSchema)
-
-module.exports = AboutUs
--- app/models/example.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose')
-var ExampleSchema = require('../schemas/example')
-var Example = mongoose.model('Example',ExampleSchema)
-
-module.exports = Example
